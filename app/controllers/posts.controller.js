bfbfe60aef084849f2c4b715bc3d61b23c44f91e
Zesheng Chen <chenz@pfw.edu>
2023-10-26 13:53:48 -0400

    4. All Posts

---
+++ app/controllers/posts.controller.js
@@ -0,0 +1,15 @@
+module.exports = {
+    
+    // show all posts
+    showPosts: (req, res) => {
+      // create dummy posts
+      const posts = [
+        { name: 'Homework', slug: 'homework', description: 'Homework is challenging!'},
+        { name: 'Lecture', slug: 'lecture', description: 'Lecture is fun!'},
+        { name: 'Project', slug: 'project', description: 'Course Project is great!'}
+      ]
+
+      // return a view with data
+      res.render('pages/posts', { posts: posts });
+    }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,15 @@
 // create a new express routes
 const express = require('express'),
     router = express.Router(),
-    mainController = require('./controllers/main.controller')
+    mainController = require('./controllers/main.controller'),
+    postController = require('./controllers/posts.controller')
 
 // export reroutes
 module.exports = router 
 
 // define routes
+// main routes
 router.get('/', mainController.showHome)
+
+// post routes
+router.get('/posts', postController.showPosts)
