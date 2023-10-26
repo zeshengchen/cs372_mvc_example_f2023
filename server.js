47e5384e328f8f51f8169011b95cac831fbdc4ab
Zesheng Chen <chenz@pfw.edu>
2023-10-26 14:19:59 -0400

    6. MongoDB Start

--- server.js
+++ server.js
@@ -2,9 +2,19 @@
 const express = require('express'),
     app = express(),
     expressLayouts = require('express-ejs-layouts'),
+    mongoose = require('mongoose'),
     port = process.env.PORT || 8080 
 
 // configure our application
+
+// connect to our database
+mongoose.connect('mongodb://127.0.0.1:27017/cs372-posts')
+    .then(() => console.log('DB connected!'))
+    .catch(() => {
+        console.log('Cannot connect to MongoDB!')
+        process.exit(1)
+    })
+
 app.use(express.static(__dirname + '/public'))
 
 // set ejs as our templating engine
